d11224fe1ef32bfadbdf989f938c775e5f632d52
Kevin Prasetyo <kevinprasetyokevin@gmail.com>
2023-09-20 16:20:35 +0700

    bismillah5

--- apps/authentication/routes.py
+++ apps/authentication/routes.py
@@ -118,10 +118,11 @@ def route_default():
 
 @blueprint.route('/reset', methods=['GET', 'POST'])
 def reset():
+    
     email = request.args.get('email', None)
     user = Users.query.filter_by(email=email).first() 
     id = request.args.get('id', None)
-    if id is None:
+    if id is None or email is None:
         return render_template('accounts/req_reset.html', msg='Link tidak valid')
     elif user:
         if user.id == int(id):
