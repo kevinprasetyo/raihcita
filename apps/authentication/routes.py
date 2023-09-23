07051f6aa9e9a0494bc8256f5667e290e3b3cb04
Kevin Prasetyo <kevinprasetyokevin@gmail.com>
2023-09-24 05:36:56 +0700

    astagfirullah2

--- apps/authentication/routes.py
+++ apps/authentication/routes.py
@@ -194,7 +194,7 @@ def gantips():
         password = hash_pass(password)
         user.password = password
         db.session.commit()
-        return redirect('/login')
+        return redirect('/login', msg='Password berhasil diganti. Silahkan login.')
     else:
         return render_template('accounts/req_reset.html')
 
