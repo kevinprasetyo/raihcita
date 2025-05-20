e568c193308b6e4e357fa88bd7e0265860283cb1
Kevin Prasetyo <kevinprasetyokevin@gmail.com>
2025-05-20 16:24:08 +0700

    alhamdulillah

--- apps/home/routes.py
+++ apps/home/routes.py
@@ -333,6 +333,27 @@ def route_template(template):
         return render_template('home/page-500.html'), 500
 
 
+@blueprint.route('/private/<template>')
+def private_template(template):
+
+    try:
+
+        if not template.endswith('.html'):
+            template += '.html'
+
+        # Detect the current page
+        segment = get_segment(request)
+
+        # Serve the file (if exists) from app/templates/home/FILE.html
+        return render_template("home/" + template, segment=segment)
+
+    except TemplateNotFound:
+        return render_template('home/page-404.html'), 404
+
+    except:
+        return render_template('home/page-500.html'), 500
+
+
 # Helper - Extract current page name from request
 def get_segment(request):
 
